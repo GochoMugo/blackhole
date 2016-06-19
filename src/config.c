a0c650f634c4e64a19ec5978cb4ce8b6b45b0aee
GochoMugo <mugo@forfuture.co.ke>
2016-06-19 09:45:24 +0300

    [config] Own the 'rootdir' arg in `bh_config_new`, by copying

--- src/config.c
+++ src/config.c
@@ -23,7 +23,12 @@ bh_config_new(bh_config **config, const char* rootdir) {
     c->runstate_path = NULL;
     c->dict = NULL;
 
-    bh_path = path_join(rootdir, BH_PATH_ROOTDIR);
+    /* ensure we own the 'path' string */
+    ret_code = asprintf((char**) &(c->path), "%s", rootdir);
+    return_err_ext(ret_code, BH_DAEMONERR_PATHRESLV);
+    ret_code = 0;
+
+    bh_path = path_join(c->path, BH_PATH_ROOTDIR);
     config_path = path_join(bh_path, BH_PATH_CONFIG);
 
     c->dict = iniparser_load(config_path);
@@ -31,7 +36,6 @@ bh_config_new(bh_config **config, const char* rootdir) {
         return_err_now(BH_CONFIGERR_LOADFILE);
     }
 
-    c->path = rootdir;
     c->name = iniparser_getstring(c->dict, "daemon:name", BH_DAEMON_DEFAULT_NAME);
     c->email = iniparser_getstring(c->dict, "daemon:email", BH_DAEMON_DEFAULT_EMAIL);
     c->runstate_path = path_join(bh_path, iniparser_getstring(c->dict, "runstate:path", ""));
@@ -40,7 +44,7 @@ bh_config_new(bh_config **config, const char* rootdir) {
 
     goto cleanup;
 on_error:
-    if (NULL != c) bh_config_free(c);
+    if (NULL != c) bh_config_free(&c);
     goto cleanup;
 cleanup:
     if (NULL != bh_path) free(bh_path);
@@ -55,8 +59,8 @@ bh_config_free(bh_config **config) {
     if (NULL == c) return;
     /** config->name, config->email are freed with `iniparser_freedict()` below */
     if (NULL != c->dict) iniparser_freedict((dictionary *) c->dict);
-    if (NULL != c->path) free(c->path);
-    if (NULL != c->runstate_path) free(c->runstate_path);
+    if (NULL != c->path) free((char*) c->path);
+    if (NULL != c->runstate_path) free((char*) c->runstate_path);
     free(c);
     *config = NULL;
 }
