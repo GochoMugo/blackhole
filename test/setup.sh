e3c5b7d4d50df36b619a2ccf0e19073c3468ab3e
GochoMugo <mugo@forfuture.co.ke>
2016-06-20 11:47:42 +0300

    [test] Compile executables test artefacts with ext '.out'

--- test/setup.sh
+++ test/setup.sh
@@ -18,12 +18,16 @@ done
 chmod -r data/bh-counter/.blackhole/counters/unreadable.counter
 
 
-noop="data/noop"
-if [[ ! -e "${noop}" ]]
-then
-    ${CC} -o "${noop}" "${noop}.c"
-fi
 
 
 # remove counters incremented in previous test runs
 rm -f data/bh-counter/.blackhole/counters/*.tmp.counter
+exes=(
+    "data/noop"
+    "data/bh-status/run"
+)
+for exe in ${exes[@]}
+do
+    [[ -e "${exe}.out" ]] && [[ "${exe}.out" -nt "${exe}.c" ]] && continue
+    ${CC} -o "${exe}.out" "${exe}.c" ${CFLAGS} ${LIBS}
+done
