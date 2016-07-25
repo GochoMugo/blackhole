92d783081b7255c6870cda320e29ea7119e7c97d
GochoMugo <mugo@forfuture.co.ke>
2016-07-25 14:53:31 +0300

    [script] echo-task: Append '+' if multiple files specified
    Feature:
    
        Since a compilation run may comprise of multiple files,
        we append a plus sign (+), to convey this meaning
        fact to the end user, that is, the files are being
        handled in a single task.
    
    References:
    
        * Bash Arrays: http://www.cyberciti.biz/faq/finding-bash-shell-array-length-elements/
        * Using Bash Arrays: http://www.techrepublic.com/article/using-arrays-in-bash/

--- script/echo-task.sh
+++ script/echo-task.sh
@@ -2,11 +2,16 @@
 # echo task being run in the Makefile
 
 task=${1}
-for path in ${@:2}
+files=(${@:2})
+len=${#files[@]}
+
+for (( i=0, first=0, last=1; i<$len; i++, first=1, last=($len-$i-2) ))
 do
-    real_path=$(readlink -f "${path}")
+    real_path=$(readlink -f "${files[$i]}")
     rootdir=$(dirname "${BASH_SOURCE[0]}")
     rootdir=$(dirname "${rootdir}")
     rootdir=$(readlink -f "${rootdir}")
-    echo -e "    \033[0;32m${task} \033[1;37m${real_path#${rootdir}/}\e[0m"
+    endlabel="+"
+    if [[ "${len}" -eq 1 ]] || [[ "${last}" -eq 0 ]] ; then endlabel=""; fi
+    echo -e "    \033[0;32m${task} \033[1;37m${real_path#${rootdir}/}\e[0m ${endlabel}"
 done
