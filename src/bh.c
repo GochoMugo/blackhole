dd6187655a5af1a05489f65c0b129de4b9d35e7c
GochoMugo <mugo@forfuture.co.ke>
2016-07-25 15:22:04 +0300

    [bh] Add flag '--version-full' for full version information
    References:
    
        * TODO Issue: https://github.com/GochoMugo/blackhole/issues/6

--- src/bh.c
+++ src/bh.c
@@ -11,12 +11,21 @@ main(int argc, const char **argv) {
     char *path = getenv("PWD");
     bool do_status = 0;
     bool do_sync = 0;
+    bool do_version_full = 0;
     bh_daemon *daemon = NULL;
 
     flag_str((const char**) &path, "path", "path to a blackhole directory");
     flag_bool(&do_status, "status", "show status");
     flag_bool(&do_sync, "sync", "run sync");
+    flag_bool(&do_version_full, "version-full", "show full version information");
     flag_parse(argc, argv, BH_VERSION);
+
+    if (0 != do_version_full) {
+        printf("Blackhole:    %s\n", BH_VERSION);
+        printf("LibGit2:      %s\n", LIBGIT2_VERSION);
+        printf("LibIniparser: %s\n", LIBINIPARSER_VERSION);
+        return_ok(ret_code);
+    }
 
     ret_code = bh_daemon_new(&daemon, path);
     return_err(ret_code);
