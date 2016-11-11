182fcf3148af913c2ddc461bda0a04b71041612d
GochoMugo <mugo@forfuture.co.ke>
2016-11-11 13:10:31 +0300

    [main] Remove redundant code

--- src/main.h
+++ src/main.h
@@ -4,11 +4,10 @@
  */
 #ifndef _BH_main_h_
 #define _BH_main_h_ 1
-#define BH_VERSION "0.0.0-develop"
 
 #define _GNU_SOURCE
-#define true 1
-#define false 0
+
+#define BH_VERSION "0.0.0-develop"
 
 #define BH_DAEMON_DEFAULT_NAME "blackhole-daemon"
 #define BH_DAEMON_DEFAULT_EMAIL "blackhole-daemon@local"
@@ -21,13 +20,6 @@
 #define BH_EVENT_PUSHORIGINERRORS "push-origin.errors"
 #define BH_EVENT_PUSHREMOTEERRORS "push-remote.errors"
 #define BH_EVENT_FATALERRORS "fatal.errors"
-
-#define _on_error goto cleanup; on_error:
-#define _cleanup goto cleanup; cleanup:
-#define return_ok(ret_code) { ret_code = 0; bh_error_clear(); goto cleanup; }
-#define return_err(result) if (0 > result) { ret_code = result; bh_error_set(ret_code); goto on_error; }
-#define return_err_ext(result, custom_err) if (0 > result) { ret_code = custom_err; bh_error_set(ret_code); goto on_error; }
-#define return_err_now(custom_err) { ret_code = custom_err; bh_error_set(ret_code); goto on_error; }
 
 #include <assert.h>
 #include <errno.h>
